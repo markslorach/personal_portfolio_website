5f08ef86d2673af310d3cc66269d9b65e6c9be4a
Mark Slorach <hello@markslorach.com>
2023-10-11 16:02:53 +0100

    update

--- app/(site)/components/Footer.jsx
+++ app/(site)/components/Footer.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer-container">
-      <div className="flex gap-2 text-2xl"></div>
       <p className="footer-text">
         Made with<span> ❤︎ </span>using Next JS and Tailwind CSS.
       </p>

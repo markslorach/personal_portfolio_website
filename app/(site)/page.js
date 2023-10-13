a2a72126c33913f271b4d22f3e525e8d7f1ee9ef
Mark Slorach <hello@markslorach.com>
2023-10-13 16:05:44 +0100

    update

--- app/(site)/page.js
+++ app/(site)/page.js
@@ -16,7 +16,7 @@ const Home = () => {
   return (
     <main>
     <NavBar/>
-      <div className="flex justify-center pt-40 mx-5">
+      <div className="flex justify-center py-24 mx-5">
         <div className="w-[680px] flex flex-col">
           <Hero />
           <CodeBox />

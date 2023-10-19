409bffcb679ba9f3b1abeacec2cac1fc0f8cceda
Mark Slorach <hello@markslorach.com>
2023-10-19 15:13:56 +0100

    added profile

--- app/(site)/profile/page.jsx
+++ app/(site)/profile/page.jsx
@@ -5,69 +5,64 @@ const ProfilePage = () => {
   return (
     <div className="flex justify-center my-24 mx-5">
       <div className="w-[680px] flex flex-col">
-      <Hero/>
-        <section className="mt-20">
-          {/* <div className="flex gap-14 items-center">
-            <div>
-              <h2 className="text-3xl sm:text-4xl font-semibold tracking-wide pb-20 dark-text-primary">
-                Hello! <span>👋🏻</span> I'm Mark. <span className="text-blue-400">A recent graduate and full-stack software developer. </span>I love creating user-friendly applications with a focus on visual and
-          system design.
-              </h2>
-            </div>
-         
-          </div> */}
-          <h2 className="text-2xl tracking tracking-wide font-semibold text-blue-400">
-            Who am I?
-          </h2>
-          <p className="tracking-wide text-base font-light leading-relaxed">
-            I am a full stack software developer who loves to create beautiful
-            and functional web applications. I have a passion for design and
-            user experience, and I enjoy solving challenging problems with code.
-            I am always eager to learn new technologies and improve my skills.
+        <Hero />
+        <section className="mt-20 dark-text-primary text-base font-light tracking-wide leading-relaxed">
+          <p className="text-base font-light tracking-wide leading-relaxed mb-5">
+            I am a recent graduate from a 16 week software development bootcamp
+            at CodeClan. During the course, I built a solid understanding of the
+            fundamentals of programming through Python,{" "}
+            <span className="font-semibold">JavaScript</span> and Java as well
+            as working with Test Driven Development,{" "}
+            <span className="font-semibold">Frameworks</span> and{" "}
+            <span className="font-semibold">Databases.</span>
           </p>
-          <h2 className="text-2xl tracking tracking-wide font-semibold dark-text-secondary py-4">
-            How did I become a developer?
-          </h2>
-          <p className="tracking-wide text-base font-light leading-relaxed">
-            I started my journey as a developer by joining CodeClan, a 16 week
-            intensive software development bootcamp. There, I learnt the
-            fundamentals of programming, web development, and software
-            engineering. I gained experience in using various languages and
-            frameworks, such as Python, Flask, Javascript, React, PostgreSQL,
-            MongoDB, and Java. I also learnt how to work in an agile
-            environment, write tests, debug errors, and deploy applications.
+          <p className="mb-5">
+            Drawn to the visual and system design aspect of building
+            user-friendly applications. The intensity of the course allowed me
+            to pick up skills quickly and demonstrate my understanding through
+            creating{" "}
+            <span className="font-semibold text-blue-400">
+              full-stack projects
+            </span>
+            .
           </p>
-          <h2 className="text-2xl tracking tracking-wide font-semibold dark-text-secondary py-4">
-            What am I working on now?
-          </h2>
-          <p className="tracking-wide text-base font-light leading-relaxed">
-            Currently, I am exploring the latest features of Next.js 13, a React
-            framework that enables fast and easy web development. I am also
-            learning how to use tailwind, a utility-first CSS framework that
-            helps me create stunning designs with minimal code. Additionally, I
-            am experimenting with different CMS systems, such as Contentful and
-            Sanity, to manage and deliver content for my web projects.
+          <p className="mb-5">
+            Curently, I am enjoying working with and learning about{" "}
+            <span className="font-semibold">Next.js 13</span>,
+            <span className="font-semibold"> Tailwind CSS</span> and how to
+            incorporate{" "}
+            <span className="font-semibold">Content Management Systems</span> to
+            create dynamic content. I love the speed, modularity and flexibility
+            frameworks provide.
           </p>
-          <h2 className="text-2xl tracking tracking-wide font-semibold dark-text-secondary py-4">
-            What did I do before becoming a developer?
-          </h2>
-          <p className="tracking-wide text-base font-light leading-relaxed">
-            Before I decided to pursue a career in software development, I
-            worked as a freelance videographer and drone operator for six years.
-            I had the opportunity to work with various clients and create
-            content for their brands and businesses. This was a creative role
-            that required me to think outside the box and overcome technical
-            difficulties.
+          <p className="mb-5">
+            Before my career change, I worked as a{" "}
+            <span className="font-semibold">
+              freelance videographer and drone operator
+            </span>{" "}
+            for 6 years. In this role, I worked closely with clients to deliver
+            creative content for brands and businesses all around Europe.
           </p>
-          <h2 className="text-2xl tracking tracking-wide font-semibold dark-text-secondary py-4">
-            What am I looking for now?
-          </h2>
-          <p className="tracking-wide text-base font-light leading-relaxed">
-            Now that I have completed my bootcamp and built a portfolio of web
-            projects, I am looking for my first role in the tech industry. I am
-            interested in joining a team of passionate developers who share my
-            enthusiasm for creating amazing web applications. I am open to
-            learning new technologies and taking on new challenges.
+          <p className="mb-5">
+            In my spare time, you’ll usually find me travelling around the
+            incredible parts of Scotland with my drone and camera. I’ll never
+            get tired of visiting Glencoe!
+          </p>
+          <p>
+            I’m now looking for my first role in the tech industry where I can
+            bring my{" "}
+            <span className="font-semibold">enthusiasm and creativity</span> and
+            continue to learn and grow as a developer. I'm eager to work with a
+            team who are building
+            <span className="font-semibold">
+              {" "}
+              innovative and user-friendly applications
+            </span>
+            .
+          </p>
+          <p className="mt-20 text-2xl font-semibold">
+            If you have a project in mind or a role you think I’d be suited for
+            I’d love to <span className=" text-gradient">hear from you</span>.
           </p>
         </section>
       </div>
@@ -77,8 +72,12 @@ const ProfilePage = () => {
 
 export default ProfilePage;
 
-   {/* <div className="avatar">
+{
+  /* <div className="avatar">
               <div className="w-28 rounded-full">
                 <img src="/images/ProfileImage.png" />
-              </div> */}
-            {/* </div> */}
+              </div> */
+}
+{
+  /* </div> */
+}
--- sanity/schemas/project-schema.js
+++ sanity/schemas/project-schema.js
@@ -65,7 +65,4 @@ const projects = {
   ],
 };
 
-
-
 export default projects;
-

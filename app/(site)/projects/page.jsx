2325e7b1863f2789b04e90d9bc392dcff376d140
Mark Slorach <hello@markslorach.com>
2023-10-25 15:25:11 +0100

    update

--- app/(site)/projects/page.jsx
+++ app/(site)/projects/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import ProjectList from "../components/ProjectList";
+import Link from "next/link";
 
 export const revalidate = 0;
 
@@ -8,10 +9,13 @@ const Projects = () => {
   return (
     <div className="flex justify-center my-28 mx-5">
       <div className="w-[680px] flex flex-col">
-        <h2 className=" text-xl sm:text-3xl font-semibold light-text-primary dark-text-primary tracking-wide">
+        <h2 className=" text-xl sm:text-3xl mb-3 font-semibold light-text-primary dark-text-primary tracking-wide">
           Projects
         </h2>
+        <p className="dark-text-secondary text-[16px] sm:text-[17px] tracking-wide leading-relaxed font-light ">Some of the recent projects I've worked on in my spare time and during my studies. Click on a project card to view more information about the project. If you have any questions please feel free to  <Link href={"/contact"}><span className="text-gradient font-semibold">get in touch</span></Link>.</p>
+        <div className="mt-20">
         <ProjectList showAll />
+        </div>
       </div>
     </div>
   );

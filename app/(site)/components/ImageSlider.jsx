dfe9a4b935a42f9708247d6d5fdf0d0fe75e57d4
Mark Slorach <hello@markslorach.com>
2023-10-28 14:55:20 +0100

    update

--- app/(site)/components/ImageSlider.jsx
+++ app/(site)/components/ImageSlider.jsx
@@ -40,26 +40,28 @@ export default function ImageSlider({ images }) {
       animate="show"
       variants={animateImageIn}
     >
-      <img src={images[0].asset.url} className="w-full" alt="Feature" />
+      <img src={images[index].asset.url} className="w-full" alt="Feature" />
 
       <motion.button
         initial={{ opacity: 0 }}
-        animate={{ opacity: 0.7 }}
+        animate={{ opacity: index > 0 ? 0.7 : 0 }}
         exit={{ opacity: 0, pointerEvents: "none" }}
-        whileHover={{ opacity: 1 }}
+        whileHover={{ opacity: index > 0 ? 1 : 0 }}
         className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-sm bg-white/50"
         onClick={() => setIndex(index - 1)}
+        disabled={index === 0}
       >
         <RiArrowLeftSFill className="h-6 w-6 text-slate-800/70" />
       </motion.button>
 
       <motion.button
         initial={{ opacity: 0 }}
-        animate={{ opacity: 0.7 }}
+        animate={{ opacity: index + 1 < images.length ? 0.7 : 0 }}
         exit={{ opacity: 0, pointerEvents: "none" }}
-        whileHover={{ opacity: 1 }}
+        whileHover={{ opacity: index + 1 < images.length ? 1 : 0 }}
         className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-sm bg-white/50"
         onClick={() => setIndex(index + 1)}
+        disabled={index + 1 >= images.length}
       >
         <RiArrowRightSFill className="h-6 w-6 text-slate-800/70" />
       </motion.button>
